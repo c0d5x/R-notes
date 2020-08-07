80e0f66811fba2c867ec2da8eb89147cf8a079ab
Jose Hidalgo <jose.hidalgo@xogito.com>
2020-08-07 00:44:42 -0600

    covid initial

---
+++ covid/ramikrispin.R
@@ -0,0 +1,14 @@
+library(coronavirus)
+library(dplyr)
+library(tidyr)
+
+
+update_dataset()
+
+summary_df <- coronavirus %>% 
+  filter(type == "confirmed") %>%
+  group_by(country) %>%
+  summarise(total_cases = sum(cases)) %>%
+  arrange(-total_cases)
+
+
