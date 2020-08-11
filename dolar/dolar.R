4d3f2cdb63c4201496cf058af1497925b7bc07c5
Jose Hidalgo <jose.hidalgo@xogito.com>
2020-08-11 11:05:17 -0600

    update

--- dolar/dolar.R
+++ dolar/dolar.R
@@ -15,24 +15,30 @@ dolar2020 <- dolar[dolar$Date > '2020-01-01',]
 
 # draw tendency with 2 methods
 ggplot(dolar2019, aes(Date, Sell, group=1)) +
-  ggtitle("From 2019") +
+  ggtitle("Desde 2019") +
   geom_step() +
+  labs(y="Venta") +
   geom_smooth(method = 'loess') +
   geom_smooth(method = 'gam')
+ggsave(file="dolar-2019.pdf", width=12, height=6, dpi=300)
 
 # adding breaks
 ggplot(dolar2020, aes(Date, Sell, group=1)) +
-  ggtitle("2020: Sell Price") +
+  ggtitle("2020: Venta") +
+  labs(y="Venta") +
   geom_step() +
   geom_smooth(method = 'loess') +
   geom_smooth(method = 'gam') +
   scale_x_date(breaks = "1 month", minor_breaks = "1 week", labels=date_format("%b"))
+ggsave(file="dolar-2020.pdf", width=12, height=6, dpi=300)
 
 buyColor <- "#69b3a2"
 sellColor <- rgb(0.2, 0.6, 0.9, 1)
 
 ggplot(dolar2020, aes(x=Date)) +
-  ggtitle("Buy / Sell") +
+  ggtitle("2020: Compra y Venta") +
   geom_step( aes(y=Buy), size=1, color=buyColor) +
   geom_step( aes(y=Sell), size=1, color=sellColor) +
+  labs(y="Compra vs Venta") +
   scale_x_date(breaks = "1 month", minor_breaks = "1 week", labels=date_format("%b"))
+ggsave(file="dolar-2020-compraventa.pdf", width=12, height=6, dpi=300)
