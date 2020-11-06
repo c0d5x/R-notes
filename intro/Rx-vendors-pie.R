38006873835856a4f0cfbaa22b43302f990f5cc4
Jose Hidalgo <jose.hidalgo@xogito.com>
2020-11-05 23:50:59 -0600

    add Rx

---
+++ intro/Rx-vendors-pie.R
@@ -0,0 +1,17 @@
+# generated with
+# cat all-recursive.txt|cut -c 32- > objects.txt
+# cat objects.txt|awk -F/ '{print $2}'|sort -u>vendors.txt
+# for vendor in $(cat vendors.txt); do
+#   aws s3 ls --recursive s3://costs.prod.recruitics.com/vendor/$vendor > $vendor.txt
+#   wc -l $vendor.txt
+# done
+# wc -l *.txt|sort -nr>Rx-vendors.txt
+# vim Rx-vendors.txt  # to replace spaces with , and add header files,vendor
+
+library(tidyverse)
+# using header
+totals <- read.csv("Rx-vendors-pie.csv", header=TRUE)
+totals$vendor <- as.factor(totals$vendor)
+summary(totals)
+
+pie(totals$files,totals$vendor)
